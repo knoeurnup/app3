a67fa3f0ce6b61d2ea3cc383cc76069ff2519112
Noeurn Krol <noeurnkrol@gmail.com>
2024-06-08 20:51:59 +0700

    add app3 with jenkins: verion : 1.0.3

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.get('/', function(req, res){
 	res.send("Hello app3");
 });
 
+app.get('/hello', function(req, res){
+	res.send("Welcomn is deployed from app3");
+});
+
+
+app.get('/hi', function(req, res){
+	res.send("Say hi from app3");
+});
+
 
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
